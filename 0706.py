b1cc4a0b4bfbf0c0091dbba3101585e3fdf5cc10
juyomo <juyomo@users.noreply.github.com>
2025-07-07 09:33:03 +0900

    Create 0706.py

---
+++ 0706.py
@@ -0,0 +1,58 @@
+### Print
+print("===== Print Statements ====")
+print("Hello!")
+print(1 + 3)
+
+
+### Variables & Data Types
+print("===== Variables & Data Types ====")
+
+var1 = 3
+print(var1)
+
+var2 = "I'm a string"
+print(var2)
+
+var3 = True
+print(var3)
+
+print("--- Integer vs. String:")
+num3 = 33
+num4 = 44
+print(num3 + num4)
+
+str3 = "33"
+str4 = "44"
+print(str3 + str4)
+
+print("--- Comparison:")
+print(1 == 1)
+print(1 > 2)
+num1 = 1
+print(num1 > 2)
+num1 = 3
+print(num1 > 2)
+
+
+### Integer Operations
+print("===== Integer Operations =====")
+print(2 + 4)
+print(2 - 3)
+print(3 * 100)
+
+print("--- Division")
+print(5 / 2)
+print(5 // 2)
+print(5 % 2)
+
+print(12 / 3)
+print(12 // 3)
+print(12 % 3)
+
+
+### Exercise
+print("===== Exercise =====")
+name = "Juyoung"
+print(name)
+print("Hi, my name is "  + name)
+print("")
