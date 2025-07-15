ac9fd1a812a0be5d534ddd11cba9d08c9c6ff29a
juyomo <juyomo@users.noreply.github.com>
2025-07-16 03:25:27 +0900

    Create 0713.py

---
+++ 0713.py
@@ -0,0 +1,149 @@
+# Author: Juyoung Moon
+
+# print("hi")
+
+# This is a comment
+# that the computer will not execute.
+# This is only for human eyes!
+
+print("")
+print("================")
+print("")
+
+print("hiiiiiiiii")
+
+if 2 == 2:
+	print("two is equal to two")
+	print("blahhhh")
+	print("blahhhh!!!!!")
+
+if 2 == 3:
+	print("two is equal to three")
+	print("=22222222222=")
+
+print("")
+print("================")
+print("")
+
+x = 3
+print("the value of x is:")
+print(x)
+
+if x < 0:
+	print("x is positive")
+
+if x > 0:
+	print("x is negative")
+
+print("")
+print("================")
+print("")
+
+x = 3
+
+print("the value of x is:")
+print(x)
+print("the value of x % 2 is:")
+print(x % 2)
+
+if x % 2 == 1:
+	print("this is an odd number")
+
+if x % 2 == 0:
+	print("this is an even number")
+
+print("")
+print("================")
+print("")
+
+y = "juyoung"
+if y == "moon":
+	print("case 111111")
+
+if y == "juyoung": # ok
+	print("case 22222")
+
+if y == 'juyoung': # ok
+	print("case 3333333")
+
+if y == "Juyoung":
+	print("case 4444444")
+
+print("")
+print("================")
+print("")
+
+y = 12 + 23
+if y == 1223:
+	print("case 111111")
+
+if y == 35:
+	print("case 22222")
+
+print("")
+print("================")
+print("")
+
+mangoExists = True
+orangeExists = False
+
+if orangeExists:
+	print("I got oranges")
+elif mangoExists:
+	print("Get mangos")
+
+print("")
+print("================")
+print("")
+
+if orangeExists:
+	print("I got oranges2222")
+else:
+	print("Get mangos2222")
+
+print("")
+print("================")
+print("")
+
+a = 0
+
+if a > 0:
+	print("It's positive!")
+elif a < 0:
+	print("It's negative!")
+else:
+	print("it's zero")
+
+
+print("")
+print("================")
+print("")
+
+b = 777777
+# Write me a program that will print
+# Seven works!
+# if b is divisible by 7
+# it will print 3
+# if b is divisible by 3
+
+if b % 7 == 0:
+	print("Seven works!")
+if b % 3 == 0:
+	print("3 works!")
+
+
+print("")
+print("================")
+print("")
+
+# Write me a program
+# that prints
+# score 90 ~ 100 -> A
+# score >= 90
+# score 50 ~ 89 -> pass
+# score < 50 -> fail
+
+print("")
+print("================")
+print("")
+
