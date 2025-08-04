2426658930c4b5a94b12a4cc9b24f36130da633e
juyomo <juyomo@users.noreply.github.com>
2025-08-04 15:18:30 +0900

    Update 0803_grades.py

--- 0803_grades.py
+++ 0803_grades.py
@@ -3,18 +3,18 @@ grade = 89.99999999
 # it's important to test your edge cases!
 
 # if grade >= 90, A
-# 89.4 -> B
 # [80, 90) B
-# >=, <=, >, <, and, or
-
 # [70, 80) C+
 # [60, 70) C
 # [50, 60) C-
 # [0 50) F
 
+# ex) 89.4 -> B
+# >=, <=, >, <, and, or
+
 if (grade >= 90):
-	print ('A')
+    print ('A')
 
 # [80, 90) B
 if (grade >= 80 and grade < 90):
-	print ('B')
+    print ('B')
